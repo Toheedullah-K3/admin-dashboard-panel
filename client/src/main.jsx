eff0b71fe59cd5f3cdf758f38319fce4b6b03b3c
Toheedullah-K3 <toheedullah002@gmail.com>
2024-11-21 17:07:59 +0500

    redux toolkit get readygit add .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,19 @@ import App from './App.jsx'
 import { configureStore } from "@reduxjs/toolkit"
 import globalReducer from "./state"
 import { Provider } from "react-redux"
+import { setUpListeners } from "@reduxjs/toolkit/query"
+import { api } from "./state/api.js"
+
 
 const store = configureStore({
   reducer: {
-    global: globalReducer
-  }
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware)
 })
+
+setUpListeners(store.dispatch);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
